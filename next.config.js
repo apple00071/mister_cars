7ad41189a377eaeb5833e508de104bf7774080ee
apple00071 <pavankumarv1497@gmail.com>
2025-05-06 18:22:09 +0530

    Updated Next.js config to prevent caching issues

--- next.config.js
+++ next.config.js
@@ -6,7 +6,30 @@ const nextConfig = {
     unoptimized: true
   },
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ],
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ],
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig; 
